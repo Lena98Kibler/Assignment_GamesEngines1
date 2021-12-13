ec9ddb03f8c4c6d5d25cf41c1adfd75daabc39b7
Lena98Kibler <C18499016@mytudublin.ie>
2021-12-13 17:43:00 +0000

    update

---
+++ WinterWonderland/Assets/SomethingBlah.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SomethingBlah : MonoBehaviour
+{
+    public float raycast_length;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Debug.DrawRay(transform.position, transform.forward * raycast_length, Color.red);
+
+    }
+}
