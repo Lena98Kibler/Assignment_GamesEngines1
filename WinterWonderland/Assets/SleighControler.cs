0d16d4759b8a5a039c3780ca44ab320edfb8943e
Lena98Kibler <C18499016@mytudublin.ie>
2021-12-14 18:38:01 +0000

    sleigh controls

--- WinterWonderland/Assets/SleighControler.cs
+++ WinterWonderland/Assets/SleighControler.cs
@@ -5,7 +5,17 @@ using UnityEngine;
 public class SleighControler : MonoBehaviour
 {
 
-    public float forwardSpeed, strafeSpeed, hoverSpeed;
+    //controlling speeds
+    public float forwardSpeed = 25f;
+    public float strafeSpeed = 7.5f;
+    public float hoverSpeed = 5f;
+
+    //storing input
+    private float activeForwardSpeed;
+    private float activeStrafeSpeed;
+    private float activeHoverSpeed;
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +25,13 @@ public class SleighControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //getting input
+        activeForwardSpeed = Input.GetAxisRaw("Vertical") * forwardSpeed;
+        activeStrafeSpeed = Input.GetAxisRaw("Horizontal") * strafeSpeed;
+        activeHoverSpeed = Input.GetAxisRaw("Hover") * hoverSpeed;
+
+        //apply input to movement
+        transform.position += transform.forward * activeForwardSpeed * Time.deltaTime;
+        transform.position += (transform.right * activeStrafeSpeed * Time.deltaTime) + (transform.up * activeHoverSpeed * Time.deltaTime);
     }
 }
