654d516176016546329dfd430146ac2ef23f794f
Lena98Kibler <C18499016@mytudublin.ie>
2021-12-14 13:18:12 +0000

    sleigh controls

--- WinterWonderland/Assets/SleighControler.cs
+++ WinterWonderland/Assets/SleighControler.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class SleighControler : MonoBehaviour
 {
+
+    public float forwardSpeed, strafeSpeed, hoverSpeed;
     // Start is called before the first frame update
     void Start()
     {
